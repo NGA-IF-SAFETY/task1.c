53f22beef25ba97971feec815eac463cd80652ba
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-12-16 00:30:21 +0700

    Create bai29.c

---
+++ bai29.c
@@ -0,0 +1,36 @@
+#include <stdio.h>  
+
+#include <conio.h>  
+
+void main ()  
+
+{
+
+   int i,j,a[5][5];
+   
+   int dem=0;
+   
+   for (i=0;i<5;i++)
+   
+    for (j=0;j<5;j++)
+    
+   {
+       dem++;
+       
+       a[j][i]=dem;
+       
+   }  
+
+   for (i=0;i<5;i++)
+   
+   {   
+   
+       for (j=0;j<5;j++)   
+       
+         printf("%4d  ",a[i][j]);   
+         
+       printf("\n");   
+       
+   }
+
+}
